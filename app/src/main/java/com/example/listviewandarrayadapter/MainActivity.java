9444f030085f7e9746d56f0618102cfc7dae93db
PavkaBrika <pavlikbrichkin@mail.ru>
2022-03-17 19:56:24 +0500

    onItemClickListener

--- app/src/main/java/com/example/listviewandarrayadapter/MainActivity.java
+++ app/src/main/java/com/example/listviewandarrayadapter/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listviewandarrayadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView countriesList = findViewById(R.id.countriesList);
+        TextView selectedText = findViewById(R.id.select);
 
         String[] countries = getResources().getStringArray(R.array.counrieslist);
 
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, countries);
 
         countriesList.setAdapter(adapter);
+
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = countries[i];
+                selectedText.setText(selectedItem);
+            }
+        });
     }
 }
